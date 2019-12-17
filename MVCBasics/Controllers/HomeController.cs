5436ed2368c6b748033f95cdbf3f491c752ae234
P N <p.newbranch@outlook.com>
2019-12-17 15:55:36 +0100

    Add project files.

---
+++ MVCBasics/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCBasics.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //HÄR ROUTAR VI TILL VÅRA MIDDLEVARE/VIEWERS
+        //här läggs Views till - vilka skall återfinnas i under katalog Views/Home/ där de skall ligga som egna "cshtml-filer"
+        //varje view fil där skall ha en IActionResult här - man skriver namnet som .cshtml-filen har och högerklickar på namnet "Company" och väljer "Add view"
+        //nya viewerna skall dyka upp som sub-objekt under "HomeController"
+
+        public IActionResult Company()
+        {
+            return View();
+        }
+
+        public IActionResult Forsale() //krångel när dessa två sista skapades - ser rätt ut men kan kanske behöva göra om?
+        {
+            return View();
+        }
+
+        public IActionResult Payment() //Glömde skriva return på denna innan jag Addade
+        {
+            return View();
+        }
+
+        public IActionResult Sold()
+        {
+            return View();
+        }
+        
+
+    }
+
+}
---
+++ MVCBasics/Startup.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace MVCBasics
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(); //denna läggs till för att tala om att vi skall använda MVC
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+
+
+            //Vi lägger här till middelware som går igenom till routing (alltså det som ligger under katalogen "wwwroot")
+            //Routern kollar
+            //    om regler blockar den så skickad den tillbaka
+            //    om de är ok så går den ner till aktuellt middelware-program
+
+            //app.UseDefaultFiles(); "JAG SKALL LETA EFTER" - Denna lades till men kommenterades sedan bort då tekniken inte fungerade längre 
+            app.UseStaticFiles();   //"Å DÄR LIGGER DEN JU" - Denna läggs till >> default opens up the wwwroot to be accessed - som nu används som en statisk server
+                                    //man öppnar allts upp det an behöver succesivt
+
+
+            //Denna klippte vi in men den fungerar inte här...  app.UseMvcWithDefaultRoute(); //sätter MVC default. 
+
+            app.UseRouting(); //"här vet jag vad jag skall göra för respons"  
+                              //hittar vi filen Wooow - vi kör den och kommer då aldrig ner i Endpoint=defultläge
+
+
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                //special routes before default
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");  //denna raden aktiverar htmlsidan vi skall ha katalog HOME och  dess INDEX
+
+                //denna kod ersätts med rad ovan
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello World!");
+                //});
+            });
+        }
+    }
+}
